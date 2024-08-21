73ff1ed18b2396e68f200e9f8e1b32b48f077425
Navaneeth Rao <67940659+Navaneethraot@users.noreply.github.com>
2024-08-21 19:35:37 +0530

    Update install-eks-kubectl.sh

--- install-eks-kubectl.sh
+++ install-eks-kubectl.sh
@@ -21,5 +21,6 @@ eksctl create cluster --config-file=eks.yaml
 
 ** Install kubens**
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 

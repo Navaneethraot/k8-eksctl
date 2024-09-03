926381b3464dbf53384d1990d912a6f7d2f38bad
Navaneeth Rao <67940659+Navaneethraot@users.noreply.github.com>
2024-09-03 19:42:10 +0530

    Update install-eks-kubectl.sh

--- install-eks-kubectl.sh
+++ install-eks-kubectl.sh
@@ -24,3 +24,6 @@ sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
+*** k9s Install***
+curl -sS https://webinstall.dev/k9s | bash
+

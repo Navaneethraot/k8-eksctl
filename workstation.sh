bf77d3233c5902c762029ab2b7798c690ed73c45
Navaneeth Rao <67940659+Navaneethraot@users.noreply.github.com>
2024-09-04 15:35:59 +0530

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -39,15 +39,15 @@ VALIDATE $? "Docker installation"
 # eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
-sudo -u eksctl version
-VALIDATE $? "eksctl installation"
+eksctl version
+#VALIDATE $? "eksctl installation"
 
 
 # kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
-VALIDATE $? "kubectl installation"
+#VALIDATE $? "kubectl installation"
 
 # kubens
 # git clone https://github.com/ahmetb/kubectx /opt/kubectx

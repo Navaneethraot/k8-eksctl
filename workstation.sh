b0d18a55b157dd69c4569acc2fc2f989431b6b95
Navaneeth Rao <67940659+Navaneethraot@users.noreply.github.com>
2024-09-05 16:22:18 +0530

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -70,3 +70,31 @@ git clone https://github.com/Navaneethraot/k8-eksctl.git
 cd k8-eksctl || { echo "Failed to change directory"; exit 1; }
 echo "Creating EKS cluster..."
 eksctl create cluster --config-file=eks.yaml
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+# sudo mv /tmp/eksctl /usr/local/bin
+
+# eksctl version
+
+
+# #kubectl
+
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.2/2024-07-12/bin/linux/amd64/kubectl
+
+# chmod +x ./kubectl
+# sudo mv kubectl /usr/local/bin/
+# kubectl version --client
+
+# -------
+# aws configure ** or use IAM
+# eksctl create cluster --config-file=eks.yaml
+
+# ** Install kubens**
+# sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+# sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+# sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+# *** k9s Install***
+# curl -sS https://webinstall.dev/k9s | bash
+
